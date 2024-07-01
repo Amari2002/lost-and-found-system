4f3b1eaa5b95e882b4cd015f8b8ed13d193737a3
Amari2002 <virtudazomaryroseann@gmail.com>
2024-07-01 22:54:10 +0800

    javadoc

---
+++ dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lost.and.found.system"}];updateSearchResults();
---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lost.and.found.system","l":"LoginFrame"},{"p":"lost.and.found.system","l":"MainFrame"}];updateSearchResults();
